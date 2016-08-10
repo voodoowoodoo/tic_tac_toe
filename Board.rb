01c3f78dee76c05d971f5e0e4a902abf99769a5e
Jorrit <jorrit_l@hotmail.com>
2016-08-10 20:12:16 +0200

    Added cell changing and checking functionality

--- Board.rb
+++ Board.rb
@@ -15,4 +15,18 @@ class Board
 	def printBoard
 		print("#{@field[0].value} | #{@field[1].value} | #{@field[2].value}\n---------\n#{@field[3].value} | #{@field[4].value} | #{@field[5].value}\n---------\n#{@field[6].value} | #{@field[7].value} | #{@field[8].value}")
 	end
+
+	#Changes the selected cell to the players symbol
+	def changeCell(cell, symbol)
+		@field[cell].value = symbol
+	end
+
+	#Checks wether the cell has been changed from the start of the game, preventing changing cells over and over
+	def cellEmpty?(cell)
+		if @field[cell].value = " "
+			return true
+		else
+			return false
+		end
+	end
 end
