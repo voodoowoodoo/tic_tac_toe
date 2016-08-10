a4c4a83ef5ffd56f033fe84a8197736b39e53216
Jorrit <jorrit_l@hotmail.com>
2016-08-10 20:42:12 +0200

    Setting up game menu

--- Game.rb
+++ Game.rb
@@ -1,1 +1,11 @@
+require "./Board"
+require "./Player"
+
 #Game class containing information about turn sequence and game setup
+class Game
+	
+	#Prints welcoming screen and options menu
+	def initialize
+		print "+++++++++++++++++++++++/nWelcome to Tic Tac Toe!/n+++++++++++++++++++++++/n/nMake a selection!/n1) Play/n2)Quit/n9)Information"
+		selection = gets.chomp
+		
